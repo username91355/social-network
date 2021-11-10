d78c8201312fa1e4fb3d3fb3ca17cf4f424b5f08
username91355 <personalmailD13@gmail.com>
2021-11-10 22:28:56 +0600

    v.0.0.4 Refactor profile page (add info) Add test for reducers.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 import React from 'react';
-import { Route } from 'react-router';
 import './App.css';
-import SideBar from './components/SideBar/SideBar';
-import FriendsPageContainer from './components/FriendsPage/FriendPageContainer';
-import MessagesPageContainer from './components/MessagesPage/MessagesPageContainer';
-import logo from './assets/img/logo.png'
+import {HeaderContainer} from "./components/common/Header/HeaderContainer";
+import {Navigation} from "./components/common/Navigation/Navigation";
+import {Route} from "react-router-dom";
+import {ProfileContainer} from "./components/Profile/ProfileContainer";
+import {FriendsContainer} from "./components/Friends/FriendsContainer";
+import {MessagesContainer} from "./components/Messages/MessagesContainer";
+
 
 function App() {
-  return (
-    <div className="app__wrapper">
-      <div className="header__wrapper">
-        <div className="header__content">
-        <div className="header__logo">
-          <img className="header__logo_img"src={logo} alt="" />
+
+    return (
+        <div className="app__wrapper">
+            <HeaderContainer />
+            <div className="app__container">
+                <Navigation/>
+                <div className="app__content">
+                    <Route path={'/profile'} render={() => <ProfileContainer />}/>
+                    <Route path={'/messages'} render={() => <MessagesContainer />}/>
+                    <Route path={'/friends'} render={() => <FriendsContainer />}/>
+                </div>
+            </div>
         </div>
-        </div>
-      </div>
-      <div className="app__content">
-        <SideBar />
-        <div className="app__mainWindow">
-          <Route path='/messages' render = {() => <MessagesPageContainer/>} />
-          <Route path='/friends' render = {() => <FriendsPageContainer/>} />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux';
-import store from './redux/redux';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+        <BrowserRouter>
+            <React.StrictMode>
+                <App/>
+            </React.StrictMode>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+)
+;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
